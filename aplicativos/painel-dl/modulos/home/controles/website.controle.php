dfd276c99937f05cabc042e458724f806b36ecbe
dlepera <dlepera88@gmail.com>
2016-01-25 17:36:05 -0200

    Correção da página 'Sobre o sistema' Correção do formulário de configuração do Google Analytics

--- aplicativos/painel-dl/modulos/home/controles/website.controle.php
+++ aplicativos/painel-dl/modulos/home/controles/website.controle.php
@@ -32,46 +32,44 @@ class WebSite extends Geral\PainelDL{
     } // Fim do método index
 
 
-    public function _sobre(){
+    public function sobre(){
 	    # Visao
 	    $this->carregarHTML('sobre_sistema');
 	    $this->visao->setTitulo(TXT_PAGINA_TITULO_PAINELDL_SOBRE);
     } // Fim do método _sobre
 
 
-
-
-	/**
-	 * Obter informações do Google Analytics
-	 *
-	 * @param        $dt_inicio
-	 * @param        $dt_fim
-	 * @param string $dimensao
-	 * @param array  $metricas
-	 *
-	 * @throws \DL3Exception
-	 */
-    public function _ganalytics($dt_inicio, $dt_fim, $dimensao = 'day', $metricas = ['visits']){
+    /**
+     * Obter informações do Google Analytics
+     *
+     * @param string $dt_inicio Data de início da consulta
+     * @param string $dt_fim    Data final da consulta
+     * @param string $dimensao  Dimensão a ser utilizada para agrupar os resultados
+     * @param array  $metricas
+     *
+     * @throws \DL3Exception
+     */
+    public function gAnalytics($dt_inicio, $dt_fim, $dimensao = 'day', $metricas = ['visits']){
         # Selecionar as configurações do Google Analytics
         $m_ga = new WebM\GoogleAnalytics();
         $m_ga->selecionarPrincipal();
 
         # Conectar ao Google Analytics
-        $o_ga = new \gapi($m_ga->contaCompleta(), $m_ga->p12);
+        $o_ga = new \gapi($m_ga->contaCompleta(), $m_ga->getP12());
 
         # Retornar as informações
         $o_ga->requestReportData(
-            $m_ga->perfil_id, $dimensao, !isset($metricas) ? ['visits'] : $metricas, null, null,
+            $m_ga->getPerfilId(), $dimensao, !isset($metricas) ? ['visits'] : $metricas, null, null,
             \Funcoes::formatarDataHora($dt_inicio, 'Y-m-d'), \Funcoes::formatarDataHora($dt_fim, 'Y-m-d')
         );
 
         # Visitas
         $infos = [];
 
-        foreach( $o_ga->getResults() as $info ){
-	        $infos[] = ['dimensao' => (string)$info, 'visitas' => $info->getVisits()];
+        foreach ($o_ga->getResults() as $info) {
+            $infos[] = ['dimensao' => (string)$info, 'visitas' => $info->getVisits()];
         } // Fim foreach
 
         echo json_encode($infos);
-    } // Fim do método _ganalytics
+    } // Fim do método gAnalytics
 } // Fim da classe WebSite
